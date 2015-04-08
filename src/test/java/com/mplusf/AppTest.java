71402e75d77fb500afbfbf6b0653015eb8ef39a2
raawan <minalfulzele@gmail.com>
2015-04-08 19:38:00 +0100

    second commit

---
+++ src/test/java/com/mplusf/AppTest.java
@@ -0,0 +1,19 @@
+package com.mplusf;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void testLengthOfTheUniqueKey() {
+ 
+		App obj = new App();
+		Assert.assertEquals(36, obj.generateUniqueKey().length());
+ 
+	}
+}
